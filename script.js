cfd36b8898a246b135ccf41c7c679efabbbcf958
yashcrest <yashcrest@gmail.com>
2024-01-15 21:41:13 +1100

    Add music player functionality and styling

---
+++ script.js
@@ -0,0 +1,4 @@
+const musicPlayerBody = document.querySelector(".music-player-body");
+const playBtn = document.querySelector("#play");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
