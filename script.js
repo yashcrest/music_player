09d31919c6a9dd59419cfb9fd27bf6e35c14d86e
yashcrest <yashcrest@gmail.com>
2024-01-16 11:19:49 +1100

    Add event listener for play button

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const musicPlayerBody = document.querySelector(".music-player-body");
 const playBtn = document.querySelector("#play");
 const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
+
+playBtn.addEventListener('click', () => {
+
+})
