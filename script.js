e700eebec17286e6257c38efcf5a75b834d74998
yash shrestha <yashcrest@gmail.com>
2024-01-17 14:34:17 +1100

    Refactor HTML and JavaScript code

--- script.js
+++ script.js
@@ -2,47 +2,42 @@ const musicPlayerBody = document.querySelector(".music-player-body");
 const playBtn = document.querySelector("#play");
 const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
-const audio = document.querySelector('#audio');
-const progressContainer = document.querySelector('.progress-container');
-const progress = document.querySelector('.progress');
-const songTitlte = document.querySelector('#song-title');
-const albumnArt = document.querySelector('#albumn-art'); 
+const audio = document.querySelector("#audio");
+const progressContainer = document.querySelector(".progress-container");
+const progress = document.querySelector(".progress");
+const songTitlte = document.querySelector("#song-title");
+const albumnArt = document.querySelector("#albumn-art");
 
 //songs list
 
 // play song
-function playSong () {
-    musicPlayerBody.classList.add('play');
-    playBtn.querySelector('i.fa-solid').classList.remove('fa-play')
-    playBtn.querySelector('i.fa-solid').classList.add('fa-pause')
+function playSong() {
+  musicPlayerBody.classList.add("play");
+  playBtn.querySelector("i.fa-solid").classList.remove("fa-play");
+  playBtn.querySelector("i.fa-solid").classList.add("fa-pause");
 }
-
 
 //pause song
 function pauseSong() {
-    musicPlayerBody.classList.remove('play')
-    playBtn.querySelector('i.fa-solid').classList.add('fa-play')
-    playBtn.querySelector('i.fa-solid').classList.remove('fa-pause')
+  musicPlayerBody.classList.remove("play");
+  playBtn.querySelector("i.fa-solid").classList.add("fa-play");
+  playBtn.querySelector("i.fa-solid").classList.remove("fa-pause");
 }
 
 //next song
-function prevBtn() {
-
-}
+function prevBtn() {}
 
 //prev song
-function nextBtn() {
-
-}
+function nextBtn() {}
 
 //event listener
-playBtn.addEventListener('click', () => {
-    // getting to see, if the music is playing or paused
-    const isPlaying = musicPlayerBody.classList.contains('play'); 
+playBtn.addEventListener("click", () => {
+  // getting to see, if the music is playing or paused
+  const isPlaying = musicPlayerBody.classList.contains("play");
 
-    if (isPlaying) {
-        pauseSong();
-    } else {
-        playSong(); 
-    }
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
 });
