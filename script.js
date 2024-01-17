1c0737da1260bb7b4e86997d5d5b316e3c99e81d
yash shrestha <yashcrest@gmail.com>
2024-01-17 13:19:20 +1100

    Update song title and add music player buttons

--- script.js
+++ script.js
@@ -2,7 +2,47 @@ const musicPlayerBody = document.querySelector(".music-player-body");
 const playBtn = document.querySelector("#play");
 const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
+const audio = document.querySelector('#audio');
+const progressContainer = document.querySelector('.progress-container');
+const progress = document.querySelector('.progress');
+const songTitlte = document.querySelector('#song-title');
+const albumnArt = document.querySelector('#albumn-art'); 
 
+//songs list
+
+// play song
+function playSong () {
+    musicPlayerBody.classList.add('play');
+    playBtn.querySelector('i.fa-solid').classList.remove('fa-play')
+    playBtn.querySelector('i.fa-solid').classList.add('fa-pause')
+}
+
+
+//pause song
+function pauseSong() {
+    musicPlayerBody.classList.remove('play')
+    playBtn.querySelector('i.fa-solid').classList.add('fa-play')
+    playBtn.querySelector('i.fa-solid').classList.remove('fa-pause')
+}
+
+//next song
+function prevBtn() {
+
+}
+
+//prev song
+function nextBtn() {
+
+}
+
+//event listener
 playBtn.addEventListener('click', () => {
+    // getting to see, if the music is playing or paused
+    const isPlaying = musicPlayerBody.classList.contains('play'); 
 
-})
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong(); 
+    }
+});
